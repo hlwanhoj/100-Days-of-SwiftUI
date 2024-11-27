b2bac7816e0e59698508ed9156c42a36b4bbc4cd
hlwanhoj <hlwanhoj@gmail.com>
2024-11-28 00:41:20 +0800

    feat: Add list mode to mission list in Moonshot

--- 100DaysOfSwiftUI/Moonshot/MoonshotView.swift
+++ 100DaysOfSwiftUI/Moonshot/MoonshotView.swift
@@ -19,6 +19,7 @@ struct MoonshotFeature {
     struct State {
         let astronauts: [String: Astronaut] = (try? MoonshotHelper.loadFromFile("astronauts.json")) ?? [:]
         let missions: [Mission] = (try? MoonshotHelper.loadFromFile("missions.json")) ?? []
+        var isShowingGrid = true
         @Presents var destination: Destination.State?
     }
     
@@ -27,6 +28,8 @@ struct MoonshotFeature {
         case binding(BindingAction<State>)
         case destination(PresentationAction<Destination.Action>)
         case missionButtonTapped(Mission)
+        case listButtonTapped
+        case gridButtonTapped
     }
     
     var body: some ReducerOf<Self> {
@@ -43,6 +46,12 @@ struct MoonshotFeature {
                     MoonshotMissionFeature.State(mission: mission, astronauts: state.astronauts)
                 )
                 return .none
+            case .listButtonTapped:
+                state.isShowingGrid = false
+                return .none
+            case .gridButtonTapped:
+                state.isShowingGrid = true
+                return .none
             }
         }
         .ifLet(\.$destination, action: \.destination)
@@ -54,15 +63,57 @@ struct MoonshotView: View {
     @Bindable var store: StoreOf<MoonshotFeature>
     
     var body: some View {
+        return Group {
+            if store.isShowingGrid {
+                MoonshotGridView(missions: store.missions) { mission in
+                    store.send(.missionButtonTapped(mission))
+                }
+            } else {
+                MoonshotListView(missions: store.missions) { mission in
+                    store.send(.missionButtonTapped(mission))
+                }
+            }
+        }
+        .navigationTitle("Moonshot")
+        .toolbar {
+            if store.isShowingGrid {
+                Button("Change to List", systemImage: "list.bullet") {
+                    store.send(.listButtonTapped)
+                }
+                .tint(Color.white)
+            } else {
+                Button("Change to Grid", systemImage: "square.grid.2x2") {
+                    store.send(.gridButtonTapped)
+                }
+                .tint(Color.white)
+            }
+        }
+        .background(MoonshotTheme.darkBackground)
+        .preferredColorScheme(.dark)
+        .navigationDestination(
+            item: $store.scope(
+                state: \.destination?.mission,
+                action: \.destination.mission
+            )
+        ) { store in
+            MoonshotMissionView(store: store)
+        }
+    }
+}
+
+private struct MoonshotGridView: View {
+    let missions: [Mission]
+    let action: (Mission) -> Void
+    
+    var body: some View {
         let columns = [
             GridItem(.adaptive(minimum: 150))
         ]
-
         return ScrollView {
             LazyVGrid(columns: columns) {
-                ForEach(store.missions) { mission in
+                ForEach(missions) { mission in
                     Button {
-                        store.send(.missionButtonTapped(mission))
+                        action(mission)
                     } label: {
                         VStack {
                             Image(mission.image)
@@ -92,17 +143,35 @@ struct MoonshotView: View {
             }
             .padding([.horizontal, .bottom])
         }
-        .navigationTitle("Moonshot")
-        .background(MoonshotTheme.darkBackground)
-        .preferredColorScheme(.dark)
-        .navigationDestination(
-            item: $store.scope(
-                state: \.destination?.mission,
-                action: \.destination.mission
-            )
-        ) { store in
-            MoonshotMissionView(store: store)
+    }
+}
+
+private struct MoonshotListView: View {
+    let missions: [Mission]
+    let action: (Mission) -> Void
+    
+    var body: some View {
+        List(missions) { mission in
+            Button {
+                action(mission)
+            } label: {
+                HStack(spacing: 16) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60, height: 60)
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                            .font(.caption)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }
+            .listRowBackground(Color.clear)
         }
+        .listStyle(.plain)
     }
 }
 
